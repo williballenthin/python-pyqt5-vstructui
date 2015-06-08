80dbf4352761479f3b87b4307c5c13b156efbf8d
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-06-08 13:23:07 -0400

    0.3.0: bump version, fix setup.py to include .ui and defs/ directory

--- setup.py
+++ setup.py
@@ -14,4 +14,9 @@ setup(name="python-pyqt5-vstructui",
       url="https://github.com/williballenthin/python-pyqt5-vstructui",
       license="Apache 2.0 License",
       install_requires=["vivisect-vstruct-wb"],
-      packages=["vstructui"])
+      packages=["vstructui"],
+      package_dir={"vstructui": "vstructui"},
+      package_data={"vstructui": [
+          "vstructui.ui",
+          "defs/*.py",
+      ]})
--- vstructui/__init__.py
+++ vstructui/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1"
+__version__ = "0.3.0"
 __all__ = ["VstructViewWidget", "BasicVstructParserSet", "VstructParserSet", "get_parsers"
            "VstructInstance"]
 
