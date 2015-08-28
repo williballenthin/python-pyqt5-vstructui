ee191c05a69145487e626fe7a1c15ea50211fc2a
Willi Ballenthin <wilbal1087@gmail.com>
2015-08-28 16:29:21 -0400

    setup: fix packaging, v0.3.3

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
-from vstructui import __version__
 from setuptools import setup
 
 
 description = "PyQt5 vstruct hex viewer widget."
 setup(name="python-pyqt5-vstructui",
-      version=__version__,
+      version="0.3.3",
       description=description,
       long_description=description,
       author="Willi Ballenthin",
       author_email="willi.ballenthin@gmail.com",
       url="https://github.com/williballenthin/python-pyqt5-vstructui",
       license="Apache 2.0 License",
-      install_requires=["vivisect-vstruct-wb"],
+      install_requires=["vivisect-vstruct-wb", "python-pyqt5-hexview"],
       packages=["vstructui"],
       package_dir={"vstructui": "vstructui"},
       package_data={"vstructui": [
--- vstructui/__init__.py
+++ vstructui/__init__.py
@@ -1,4 +1,3 @@
-__version__ = "0.3.1"
 __all__ = ["VstructViewWidget", "BasicVstructParserSet", "VstructParserSet", "get_parsers"
            "VstructInstance"]
 
